93c693ed2b1c98d785160437c8fb3aa59db74043
luhh_fvl <168596833+luanafvl@users.noreply.github.com>
2024-10-23 00:07:35 -0300

    Add files via upload

---
+++ projetoSpring/src/main/java/com/luhh/projetospring/api/models/Departamento.java
@@ -0,0 +1,28 @@
+package com.luhh.projetospring.api.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Departamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nome;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
